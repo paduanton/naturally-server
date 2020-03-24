35325eb107f11fdeb5d3cd9bb840d9015ed91dee
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-24 00:52:36 -0300

    creating table             ->foreign('recipes_id')->references('id')->on('recipes');

---
+++ database/migrations/2020_03_24_004700_create_users_favorites_recipes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersFavoritesRecipesTable extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('users_favorites_recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id');
+            $table->foreignId('recipes_id');
+            $table->timestampsTz(0);
+            $table->softDeletesTz('deleted_at', 0);
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('recipes_id')->references('id')->on('recipes');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users_favorites_recipes');
+    }
+}
