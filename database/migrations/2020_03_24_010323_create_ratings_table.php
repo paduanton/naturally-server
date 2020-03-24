f590d57e46a96fc280a7770b57d5cbb05140cb12
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-24 01:18:34 -0300

    creating ratings table

---
+++ database/migrations/2020_03_24_010323_create_ratings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id');
+            $table->foreignId('recipes_id');
+            $table->boolean('made_it')->nullable();
+            $table->tinyInteger('value');
+            $table->string('description')->nullable();
+            $table->string('picture_url')->nullable();
+            $table->timestampsTz(0);
+            $table->softDeletesTz('deleted_at', 0);
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('recipes_id')->references('id')->on('recipes');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
