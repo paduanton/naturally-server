87817a273cb6853758686338dbe33408e2f0d9f4
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-24 01:24:05 -0300

    creating comments table

---
+++ database/migrations/2020_03_24_011906_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id');
+            $table->foreignId('recipes_id');
+            $table->foreignId('parent_comments_id')->nullable();
+            $table->string('description');
+            $table->timestampsTz(0);
+            $table->softDeletesTz('deleted_at', 0);
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('recipes_id')->references('id')->on('recipes');
+            $table->foreign('comments_id')->references('id')->on('comments');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
