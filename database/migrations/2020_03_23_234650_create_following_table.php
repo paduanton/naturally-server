462e25d622e569be2955e66b179edcca136942a4
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-03-23 23:53:31 -0300

    creating table following

--- database/migrations/2020_03_23_234650_create_following_table.php
+++ database/migrations/2020_03_23_234650_create_following_table.php
@@ -4,27 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOauthProvidersTable extends Migration
+class CreateFollowingTable extends Migration
 {
-    
+
     public function up()
     {
-        Schema::create('oauth_providers', function (Blueprint $table) {
+        Schema::create('following', function (Blueprint $table) {
             $table->id();
             $table->foreignId('users_id');
-            $table->enum('name', ['facebook', 'twitter', 'google']); 
-            // user name?
-            $table->string('provider_profile_url');
-            $table->string('provider_picture_url');
-            $table->unsignedBigInteger('users_provider_id')->unique();
+            $table->foreignId('following_users_id');
             $table->timestampsTz(0);
             $table->softDeletesTz('deleted_at', 0);
             $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('following_users_id')->references('id')->on('users');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('oauth_providers');
+        Schema::dropIfExists('following');
     }
 }
