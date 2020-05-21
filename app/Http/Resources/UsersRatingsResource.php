8ded94a15fd8ac29641e9898088f98cfd58d1486
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-05-21 02:25:50 -0300

    adding users ratingsrecipes resource file

---
+++ app/Http/Resources/UsersRatingsResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UsersRatingsResource extends JsonResource
+{
+
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'users_id' => $this->users_id,
+            'made_it' => $this->made_it,
+            'value' => $this->value,
+            'description' => $this->description,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'recipe' => $this->recipes
+        ];
+    }
+}
