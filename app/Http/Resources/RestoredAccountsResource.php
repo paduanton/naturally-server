499e3d06570fb39e7a4cda4fe216dbe19a129e44
Antonio de Pádua <antonio.junior@kinghost.com.br>
2020-06-09 00:50:26 -0300

    creating restore acount service and finishin resource

--- app/Http/Resources/RestoredAccountsResource.php
+++ app/Http/Resources/RestoredAccountsResource.php
@@ -6,14 +6,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RestoredAccountsResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'token' => $this->token,
+            'signature' => $this->signature,
+            'done' => (boolean) $this->done,
+            'expires_at' => $this->expires_at->toDateTimeString(),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
     }
 }
